9dbbdb529703f5f0041db5c7222ffbc5e2d6e304
Brie Carranza <hi@brie.ninja>
2020-08-05 21:49:13 -0400

    Don't write out to file

--- check-expiry.pl
+++ check-expiry.pl
@@ -7,8 +7,4 @@ my @the_domains = ("amazon.com", "apple.com","facebook.com","google.com");
 foreach my $domain (@the_domains) {
 	my $new_expiry = expire_date( $domain, '%Y-%m-%d' );
 	print "$domain: $new_expiry\n";
-	open (TEMP_FILE, ">>/tmp/whois-results.md") ||
-              &CgiDie ("Cannot open whois-results.md. Sorry.");
-        print TEMP_FILE        "  - $domain: $new_expiry\n";
-        close (TEMP_FILE);
 }
